53067db081ed6fdd955de9ac8f1464d627e809aa
marlon symczecym <marlonsymczecym.dev@gmail.com>
2021-01-12 22:57:53 -0300

    feat: show all, and show by id tasks

--- lib/tasks.ex
+++ lib/tasks.ex
@@ -2,20 +2,28 @@ defmodule Tasks do
   @moduledoc """
   Module onde tera toda estrutura das tarefas
   """
-  defstruct message: nil, concluded: false, levels: nil
+  defstruct id: 0, message: nil, concluded: false, levels: nil
   @tasks %{:directory => "files", :file => "tasks.txt"}
 
   def create(message, levels) do
-    (read() ++ [%__MODULE__{message: message, levels: levels}])
+    (read() ++ [%__MODULE__{id: id_generate() + 1, message: message, levels: levels}])
     |> :erlang.term_to_binary()
     |> write()
   end
+
+  def show_all(), do: read()
+  def show_id(id), do: read() |> Enum.find(&(&1.id == id))
 
   defp write(tasks) do
     File.write("#{@tasks[:directory]}/#{@tasks[:file]}", tasks)
   end
 
-  def read() do
+  defp id_generate() do
+    read()
+    |> Enum.count()
+  end
+
+  defp read() do
     {:ok, file} = File.read("#{@tasks[:directory]}/#{@tasks[:file]}")
 
     file
